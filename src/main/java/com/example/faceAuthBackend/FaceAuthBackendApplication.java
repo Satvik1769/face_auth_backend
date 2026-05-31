4f42e5f2f1128bb6250a911d2ba4e2d8369f5c34
teslacodes7 <satvik.manchanda@univest.in>
2026-05-31 15:47:34 +0530

    first commit

---
+++ src/main/java/com/example/faceAuthBackend/FaceAuthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.faceAuthBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceAuthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceAuthBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/faceAuthBackend/FaceAuthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faceAuthBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceAuthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
